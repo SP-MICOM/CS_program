099375203e831efdc7a01baec437b4a74353899e
SP-MICOM <superfamicom1022@gmail.com>
2026-04-23 21:43:37 +0900

    (2026.4.23) 일반화

---
+++ Program/Factory.cs
@@ -0,0 +1,31 @@
+﻿using Program;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+internal class Factory<T> where T : new()
+{
+    private T[] objects;
+    private int count;
+
+    public Factory(int size = 5)
+    {
+        objects = new T[size];
+    }
+
+    public T Instantiate()
+    {
+        if (count >= objects.Length)
+        {
+            Console.WriteLine("No more can be created");
+
+            return default(T);
+        }
+
+        T clone = new T();
+
+        objects[count] = clone;
+
+        return objects[count++];
+    }
+}
---
+++ Program/Obstacle.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+internal class Obstacle
+{
+    public int x;
+    public int y;
+
+    public void Translate(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+
+        Console.WriteLine("x : ", x, "y : ", y);
+    }
+}
