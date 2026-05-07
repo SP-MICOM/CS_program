60136f083821e64e14e4552e6529c3551ceb3aa5
SP-MICOM <superfamicom1022@gmail.com>
2026-05-07 21:44:00 +0900

    (2026.5.7) 인터페이스 분리 원칙

--- Program/Program.cs
+++ Program/Program.cs
@@ -8,7 +8,21 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            #region
+            #region 의존 역전 원칙
+            // 추상화는 세부 사항에 의존해서는 안되며, 세부 사항이
+            // 추상화에 의존할 수 있도록 설계해야 하는 원칙입니다.
+
+            Character character = new Character();
+
+            Weapon knife = new Knife();
+            Weapon grenade = new Grenade();
+
+            character.Acquire(knife);
+            character.Acquire(grenade);
+
+
+
+
             #endregion
         }
     }
